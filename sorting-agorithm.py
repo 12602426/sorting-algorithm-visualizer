04094e043e87c83d0a764e78e8deda52326c6e5a
Wouter Haringhuizen <wouterharinghuizen@hotmail.com>
2022-01-16 19:09:18 +0100

    bubblesort

--- sorting-agorithm.py
+++ sorting-agorithm.py
@@ -6,11 +6,12 @@ pygame.init()
 class ColorInformation:
     BLACK = 0, 0, 0
     SOFTWHITE = 250, 250, 250
-    RED = 255, 0, 0
+    BLUE = 0, 0, 255
     GREEN = 0, 255, 0
     LIGHTGREY = 192, 192, 192
     GREY = 110, 110, 110
     PADDINGPIX = 200
+    FONT = pygame.font.SysFont('dejavusansmono', 20)
 
     def __init__(self, width, height, unsorted_list):
         self.width = width
@@ -38,11 +39,17 @@ def generate_random_list(len, min_val, max_val):
 
 def draw(color_info):
     color_info.window.fill(color_info.SOFTWHITE)
-    drawList(color_info)
+    keycontrols = color_info.FONT.render("Hit the SPACEBAR to start sorting | R - Reset", 1, color_info.BLACK)
+    color_info.window.blit(keycontrols, (5, 5))
+    draw_list(color_info)
     pygame.display.update()
 
-def drawList(color_info):
+def draw_list(color_info, color_position=dict(), clear_plot=False):
     lst = color_info.unsorted_list
+
+    if clear_plot:
+        pygame.draw.rect(color_info.window, color_info.SOFTWHITE, (0, color_info.PADDINGPIX, color_info.height * 2, color_info.width * 2))
+
     for i, val in enumerate(lst):
         x_point = color_info.start_cor_x + i * color_info.bar_width
         y_point = color_info.height - (val - color_info.min) * color_info.bar_height
@@ -51,12 +58,18 @@ def drawList(color_info):
         if i % 2 == 1:
             color = color_info.LIGHTGREY
 
+        if i in color_position:
+            color = color_position[i]
+
         pygame.draw.rect(color_info.window, color, (x_point, y_point, color_info.bar_width, color_info.height))
+
+    if clear_plot:
+        pygame.display.update()
 
 
 
 def main():
-    run = 1
+    run = True
     clock = pygame.time.Clock()
 
     n = 30
@@ -66,18 +79,60 @@ def main():
     unsorted_list = generate_random_list(n, min_val, max_val)
     color_info = ColorInformation(800, 600, unsorted_list)
 
+    currently_sorting = False
+
+    current_algorithm = bubble_sort
+    current_algorithm_name = "bubble sort"
+    current_generator = None
+
     while run:
         clock.tick(60)
 
-        draw(color_info)
+        if currently_sorting:
+            try:
+                    next(current_generator)
+            except StopIteration:
+                    sorting = False
 
-        pygame.display.update()
+        else:
+            draw(color_info)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = 0
+                run = False
+
+            if event.type != pygame.KEYDOWN:
+                continue
+
+            if event.key == pygame.K_SPACE:
+                currently_sorting = True
+                current_generator = current_algorithm(color_info)
+
+            if event.key == pygame.K_r:
+                unsorted_list = generate_random_list(n, min_val, max_val)
+                color_info.set_list(unsorted_list)
+                currently_sorting = False
 
     pygame.quit()
+
+def bubble_sort(color_info):
+    unsorted_list = color_info.unsorted_list
+    print("im called bitch")
+
+    for i in range(len(unsorted_list) - 1):
+        for j in range(len(unsorted_list) - i - 1):
+            val1 = unsorted_list[j]
+            val2 = unsorted_list[j + 1]
+
+            if val1 > val2:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                draw_list(color_info, {j: color_info.BLUE, j + 1: color_info.GREEN}, True)
+                yield True
+
+    return unsorted_list
+
+
+
 
 if __name__ == "__main__":
     main()
